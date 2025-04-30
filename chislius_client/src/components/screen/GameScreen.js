22d1db7cd0367aac67ac633b8f7cc1dfc06c1b88
Darya-Zhulitova <darya7h@yandex.ru>
2025-04-30 21:08:18 +0300

    Add functional to simulate remote actions

--- chislius_client/src/components/screen/GameScreen.js
+++ chislius_client/src/components/screen/GameScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import Board from '../board/Board'
 import Form from '../form/Form'
 import './GameScreen.css'
@@ -6,6 +6,33 @@ import './GameScreen.css'
 function GameScreen() {
     const [canSubmit, setCanSubmit] = useState(false)
     const [cardsList, setCardsList] = useState([])
+
+    const refreshCards = useCallback(() => {
+        setCardsList(prevCards => {
+            if (prevCards.length === 0) return prevCards
+            const newCards = [...prevCards]
+            const randomIndex = Math.floor(Math.random() * newCards.length)
+            newCards[randomIndex] = {
+                ...newCards[randomIndex], isOpen: !newCards[randomIndex].isOpen
+            }
+            return newCards
+        })
+    }, [])
+
+    useEffect(() => {
+        let intervalId
+
+        setCardsList(Array.from({length: 16}, (_, i) => ({
+            id: i, isOpen: false
+        })))
+
+        refreshCards()
+        intervalId = setInterval(refreshCards, 250)
+
+        return () => {
+            if (intervalId) clearInterval(intervalId)
+        }
+    }, [refreshCards])
 
     const clearBoard = () => {
         setCardsList(cardsList.map((item) => ({...item, isOpen: false})))
