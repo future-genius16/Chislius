dac8b3436236d5c5a38134ad5d4a2837988e0fc3
Artem Fedorenko <avfedorenko3@yandex.com>
2025-06-12 21:07:19 +0300

    Delete unused files

--- chislius_server/src/main/java/ru/hse/chislius_server/dto/room/RoomResponse.java
+++
@@ -1,9 +0,0 @@
-package ru.hse.chislius_server.dto.room;
-
-import ru.hse.chislius_server.model.room.RoomState;
-import ru.hse.chislius_server.model.room.RoomType;
-
-import java.util.List;
-
-public record RoomResponse(String code, RoomType type, int capacity, RoomState state, List<String> usernames) {
-}
